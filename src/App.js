2e65b90a5945b7e0e13b628995da6cb4bb7d978f
yocosaka <sakayoco55@gmail.com>
2023-03-11 11:39:42 +0900

    Add BlueButton with mui/material styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import { Button, styled, Typography } from '@mui/material';
+import React from 'react';
+
 function App() {
-  return <div>App</div>;
+  const BlueButton = styled(Button)({
+    backgroundColor: 'skyblue',
+    color: '#888',
+    margin: 5,
+    '&:hover': {
+      backgroundColor: 'tomato',
+    },
+    '&:disabled': {
+      backgroundColor: 'grey',
+      color: 'white',
+    },
+  });
+
+  return (
+    <div>
+      <Typography variant="h1" component="h2">
+        h1. Heading
+      </Typography>
+      <BlueButton>My Button</BlueButton>
+      <BlueButton>My Button 2</BlueButton>
+    </div>
+  );
 }
 
 export default App;
